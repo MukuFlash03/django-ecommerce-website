4d39cd27a5b9d61fede026a7d35fc11a56d20bd5
Mukul Chandrakant Mahadik <mukulm2010@gmail.com>
2024-12-25 13:57:37 -0800

    Created DB models entities

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Customer(models.Model):
+	user = models.OneToOneField(User, null=True, blank=True, on_delete=models.CASCADE)
+	name = models.CharField(max_length=200, null=True)
+	email = models.CharField(max_length=200)
+
+	def __str__(self):
+		return self.name
+	
+	def __repr__(self):
+		return f"Customer(name={self.name}, email={self.email})"
+	
+class Product(models.Model):
+	name = models.CharField(max_length=200)
+	price = models.FloatField()
+	digital = models.BooleanField(default=False,null=True, blank=True)
+	# image = models.ImageField(null=True, blank=True)
+
+	def __str__(self):
+		return self.name
+	
+	def __repr__(self):
+		return f"Product(name={self.name}, price={self.price}, digital={self.digital})"
+
+class Order(models.Model):
+	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
+	date_ordered = models.DateTimeField(auto_now_add=True)
+	complete = models.BooleanField(default=False)
+	transaction_id = models.CharField(max_length=100, null=True)
+
+	def __str__(self):
+		return str(self.id)
+	
+	def __repr__(self):
+		return f"Order(customer={self.customer}, date_ordered={self.date_ordered}, complete={self.complete}, transaction_id={self.transaction_id})"
+
+class OrderItem(models.Model):
+	product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+	quantity = models.IntegerField(default=0, null=True, blank=True)
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.product.name
+	
+	def __repr__(self):
+		return f"OrderItem(product={self.product}, order={self.order}, quantity={self.quantity}, date_added={self.date_added})"
+ 
+class ShippingModel(models.Model):
+	customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+	order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+	address = models.CharField(max_length=200, null=False)
+	city = models.CharField(max_length=200, null=False)
+	state = models.CharField(max_length=200, null=False)
+	zipcode = models.CharField(max_length=200, null=False)
+	date_added = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.address
+
+	def __repr__(self):
+		return f"ShippingModel(customer={self.customer}, order={self.order}, address={self.address}, city={self.city}, state={self.state}, zipcode={self.zipcode}, date_added={self.date_added})"
--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 def store(request):
 	context = {}
 	return render(request, 'store/store.html', context)
