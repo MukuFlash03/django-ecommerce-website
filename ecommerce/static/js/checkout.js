afdedd493c0d68a2d30d24514ed12b93a3a9a62b
Mukul Chandrakant Mahadik <mukulm2010@gmail.com>
2024-12-25 23:47:49 -0800

    Paypal developer API client-side button added.

--- ecommerce/static/js/checkout.js
+++ ecommerce/static/js/checkout.js
@@ -24,9 +24,11 @@ form.addEventListener('submit', function (e) {
 	document.getElementById('payment-info').classList.remove('hidden')
 })
 
+/*
 document.getElementById('make-payment').addEventListener('click', function (e) {
 	submitFormData()
 })
+*/
 
 function submitFormData() {
 	console.log('Payment button clicked')
