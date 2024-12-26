3321d9347ac4948723a760da9db30946a403ae3e
Mukul Chandrakant Mahadik <mukulm2010@gmail.com>
2024-12-25 18:15:47 -0800

    Updated cart quantity seen in cart logo using context

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -4,8 +4,18 @@ from .models import *
 import json
 
 def store(request):
+	if request.user.is_authenticated:
+		customer = request.user.customer
+		order, created = Order.objects.get_or_create(customer=customer, complete=False)
+		items = order.orderitem_set.all()
+		cartItems = order.get_cart_items_total
+	else:
+		items = []
+		order = {'get_cart_price_total': 0, 'get_cart_items_total': 0}
+		cartItems = order['get_cart_items_total']
+
 	products = Product.objects.all()
-	context = {'products': products}
+	context = {'products': products, 'cartItems': cartItems}
 	return render(request, 'store/store.html', context)
 
 def cart(request):
@@ -13,10 +23,13 @@ def cart(request):
 		customer = request.user.customer
 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
 		items = order.orderitem_set.all()
+		cartItems = order.get_cart_items_total
 	else:
 		items = []
 		order = {'get_cart_price_total': 0, 'get_cart_items_total': 0}
-	context = {'items': items, 'order': order}
+		cartItems = order['get_cart_items_total']
+
+	context = {'items': items, 'order': order, 'cartItems': cartItems}
 	return render(request, 'store/cart.html', context)
 
 def checkout(request):
@@ -24,10 +37,13 @@ def checkout(request):
 		customer = request.user.customer
 		order, created = Order.objects.get_or_create(customer=customer, complete=False)
 		items = order.orderitem_set.all()
+		cartItems = order.get_cart_items_total
 	else:
 		items = []
 		order = {'get_cart_price_total': 0, 'get_cart_items_total': 0}
-	context = {'items': items, 'order': order}
+		cartItems = order['get_cart_items_total']
+
+	context = {'items': items, 'order': order, 'cartItems': cartItems}
 	return render(request, 'store/checkout.html', context)
 
 def updateItem(request):
